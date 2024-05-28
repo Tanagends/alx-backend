b222a9213302746cc78621f51976bb5c186abd39
Tanagends <tanagends@gmail.com>
2024-05-28 23:09:23 +0200

    babel

---
+++ 0x02-i18n/3-app.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+"""Flask application"""
+
+from flask import Flask, render_template, request
+from flask_babel import Babel, _
+
+app = Flask(__name__)
+babel = Babel(app)
+
+
+class Config:
+    """configures a flask app"""
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = 'en'
+    BABEL_DEFAULT_TIMEZONE = 'UTC'
+
+
+app.config.from_object(Config)
+
+
+@app.route('/', strict_slashes=False)
+def index():
+    """Landing page"""
+    return render_template("1-index.html")
+
+
+@babel.localeselector
+def get_locale():
+    """Determines best match with our supported languages"""
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
