18ac8ffe5a8b264c90204c656ce8bff009293166
Tanagends <tanagends@gmail.com>
2024-05-28 23:49:12 +0200

    i18n

--- 0x02-i18n/4-app.py
+++ 0x02-i18n/4-app.py
@@ -1,40 +1,47 @@
 #!/usr/bin/env python3
-"""Flask application"""
+"""
+task 4 answer
+"""
+from flask import (
+    Flask,
+    render_template,
+    request
+)
+from flask_babel import Babel
 
-from flask import Flask, render_template, request
-from flask_babel import Babel, _
+
+class Config(object):
+    """
+    Configuration for Babel
+    """
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
+
 
 app = Flask(__name__)
-babel = Babel(app)
-
-
-class Config:
-    """configures a flask app"""
-    LANGUAGES = ["en", "fr"]
-    BABEL_DEFAULT_LOCALE = 'fr'
-    BABEL_DEFAULT_TIMEZONE = 'UTC'
-
-
 app.config.from_object(Config)
-
-
-@app.route('/', strict_slashes=False)
-def index():
-    """Landing page"""
-    return render_template("1-index.html")
+babel = Babel(app)
 
 
 @babel.localeselector
 def get_locale():
-    """Determines best match with our supported languages"""
-
-    req = request.args.get('locale')
-
-    if req and req in app.config['LANGUAGES']:
-        return req
-        
+    """
+    Select and return best language match based on supported languages
+    """
+    loc = request.args.get('locale')
+    if loc in app.config['LANGUAGES']:
+        return loc
     return request.accept_languages.best_match(app.config['LANGUAGES'])
 
 
+@app.route('/', strict_slashes=False)
+def index() -> str:
+    """
+    Handles / route
+    """
+    return render_template('4-index.html')
+
+
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(port="5000", host="0.0.0.0", debug=True)
